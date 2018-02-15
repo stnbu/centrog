1089197acc8bd220538d50a86e77f6bf970103fc
Mike Burr <mburr@unintuitive.org>
2018-02-14 17:05:56 -0800

    Refactor with readable imports. Untested psycopg2 test.

--- web.py
+++ web.py
@@ -1,18 +1,20 @@
-from flask import Flask, session, redirect, url_for, escape, request
+import psycopg2
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.secret_key = ''
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
+    if 'username' in flask.session:
+        return 'Logged in as %s' % flask.escape(flask.session['username'])
     return 'You are not logged in'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+    if flask.request.method == 'POST':
+        flask.session['username'] = flask.request.form['username']
+        return flask.redirect(flask.url_for('index'))
     return '''
         <form method="post">
             <p><input type=text name=username>
@@ -20,14 +22,16 @@ def login():
         </form>
     '''
 
+@app.route('/test')
+def test():
+    conn = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
+
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
+    flask.session.pop('username', None)
+    return flask.redirect(url_for('index'))
 
-# set the secret key.  keep this really secret:
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=1234, debug=False)
+    app.run('0.0.0.0', port=1234, debug=True)
