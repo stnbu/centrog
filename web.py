7a08da8ba3dd8555a430f93b585332bcc6fd80fc
Mike Burr <mburr@unintuitive.org>
2018-02-15 23:30:06 -0800

    Apply syslogtag: (errant colon) hack. "treat" rows (inefficiently).

--- web.py
+++ web.py
@@ -1,3 +1,12 @@
+"""Serve MonitorWare schema-compliant Syslog records from a RDBMS using a simple, fast web UI.
+"""
+
+# FIXME: there is a criminally ugly hack used below having to do with syslog tagging. I haven't gotten to the bottom
+# of it yet, but it looks like python retains the ":" at the end of the tag...somehow. TBD, the below hacks hides the
+# mess from the user.
+#
+# mburr@unintuitive.org
+
 import psycopg2
 import flask
 import flask_wtf
@@ -54,6 +63,29 @@ class DBQueryForm(flask_wtf.FlaskForm):
     def validate_on_submit(self):
         return self.validate()
 
+
+def get_treated_rows(columns, rows):
+    """Brutally inefficent...
+
+    Loop through the rows and make changes...
+    """
+    priorities_dict = dict(priorities)
+    print priorities_dict
+    new_rows = []
+    for row in rows:
+        new_row = []
+        for column_name, value in zip(columns, row):
+            if column_name == 'syslogtag':
+                # FIXME: criminally ugly hack (see top of module)
+                value = value[:-1]
+            elif column_name == 'priority':
+                value = priorities_dict[str(value)]
+            new_row.append(value)
+        new_rows.append(new_row)
+
+    return new_rows
+
+
 def get_sql_query(data):
     query_template = """
         SELECT {display_columns} FROM systemevents WHERE priority = '{priority}' AND syslogtag = '{syslogtag}';
@@ -63,19 +95,24 @@ def get_sql_query(data):
     return sql
 
 @app.route('/')
-def query():
+def index():
     args = flask.request.args
     form = DBQueryForm(args)
 
     rows = []
     column_headers = []
     if form.validate_on_submit():
+        # FIXME: criminally ugly hack (see top of module)
+        form.syslogtag.data = form.data['syslogtag'] + u':'
         sql = get_sql_query(data=form.data)
         rows = get_rows(sql)
 
         c = dict(columns)
         for column_name in form.data['display_columns']:
             column_headers.append(c[column_name])
+        rows = get_treated_rows(columns=form.data['display_columns'], rows=rows)
+        # FIXME: criminally ugly hack (see top of module)
+        form.syslogtag.data = form.data['syslogtag'][:-1]
 
     return flask.render_template('index.html', form=form, column_headers=column_headers, rows=rows)
 
